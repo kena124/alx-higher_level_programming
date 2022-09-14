eecbdfd2cc271953d702fd305e8bb4c21d8fe71d
kena biratu <kenabirtu19@gmail.com>
2022-09-14 15:56:50 -0700

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for i in range(list_length):
-        c = 0
-        try:
-            c = my_list_1[i] / my_list_2[i]
-        except (ZeroDivisionError, ValueError):
-            print("division by 0")
-        except (TypeError):
-            print("wrong type")
-        except (IndexError):
-            print("out of range")
-        finally:
-            new_list.append(c)
-    return new_list
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
