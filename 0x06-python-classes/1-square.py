f5ce376f77431214206efee95d4ff940f91c30d8
kena biratu <kenabirtu19@gmail.com>
2022-09-14 15:26:41 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,5 @@
 
 class Square:
     """ The object representation of a square with size attribute """
-    def __init__(self, size=0):
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    def __init__(self, size):
         self.__size = size
