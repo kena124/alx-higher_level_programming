dc4ea4bf01ece456c109b14aef8d1980563f5ec7
kena biratu <kenabirtu19@gmail.com>
2022-10-24 12:12:46 -0700

    updated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Moduele 5-base_geometry.py
+Empty BaseGeometry
+Class
+"""
+
+
+class BaseGeometry:
+    """ Base Geometry """
+
+    def area(self):
+        """ Calculates the area of the geometry """
+        raise Exception("area() is not implemented")
