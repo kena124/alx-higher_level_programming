3d982627eb7d561b0dcf7252e2c6c8bcfb249dc8
kena biratu <kenabirtu19@gmail.com>
2022-10-24 11:47:43 -0700

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class.py
+same instance
+and methods
+"""
+
+
+def is_same_class(obj, a_class):
+    """ is the objec is exactly an instance of the specified class """
+    return type(obj) == a_class
