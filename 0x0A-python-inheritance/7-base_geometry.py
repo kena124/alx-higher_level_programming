52decc10083a4bc39d83d231cfffa95202bde851
kena biratu <kenabirtu19@gmail.com>
2022-10-24 12:14:34 -0700

    updated

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Moduele 5-base_geometry.py
+Empty BaseGeometry
+Class
+"""
+
+
+class BaseGeometry:
+    """ Base Geometry """
+
+    def area(self):
+        """ Calculates the area of the geometry """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates input
+        Args:
+        name (st): always a string
+        value (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
